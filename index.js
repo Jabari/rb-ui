487de83ba5c9cbf23908b9233f1a24ef62b01b69
Jabari <jabaricmartin@gmail.com>
2017-11-20 04:40:20 -0800

    delete uneccessary dependencies

--- index.js
+++ index.js
@@ -9,37 +9,26 @@ var compressor = require('node-minify')
 var conditionals = require('postcss-conditionals')
 var customMedia = require("postcss-custom-media")
 var syntax = require('postcss-scss')
-//cssnano
-//postcss-font-magician
-// postcss-strip-inline-comments
-
-// if (process.BROWSER_BUILD) {
-//   var Muuri = require("muuri")
-// }
 
 // css to be processed
-var css = fs.readFileSync("src/mnml.css", "utf8")
+var css = fs.readFileSync("./css/rb-styles.css", "utf8")
 
 // process css
 output = postcss()
-  .use(atImport())
-  .use(cssvariables())
-  .use(conditionals())
   .use(customMedia())
   .use(autoprefixer())
-//  .process(css, {
-  .process({
-    from: "./src/mnml.css",
-    to: "./css/mnml.css"
+  .process(css, {
+    from: "./css/rb-styles.css",
+    to: "./css/all-styles.css"
   }).then(function(output) {
-    fs.writeFile("css/mnml.css", output, 'utf-8')
+    fs.writeFile("css/all-styles.css", output, 'utf-8')
   });
 
 
 // Using Clean-css for CSS
 var compressor = require("node-minify");
-// compressor.minify({
-//     compressor: 'clean-css',
-//     fileIn: './css/mnml.css',
-//     fileOut: './css/mnml.min.css'
-// });
+compressor.minify({
+    compressor: 'clean-css',
+    fileIn: './css/all-styles.css',
+    fileOut: './css/all-styles.min.css'
+});
